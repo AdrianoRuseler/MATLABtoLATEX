f6e408e093c00f7553b71d8704d1b39fd9dd5334
Adriano Ruseler <adrianoruseler@gmail.com>
2015-02-26 15:39:36 -0300

    bode2tikz - Added suport for multi plot
    bode2tikz - Added suport for multi plot

--- bode2tikz/bode2tikz.m
+++ bode2tikz/bode2tikz.m
@@ -43,19 +43,37 @@ end
 if nargin <2 % input options not supplied
     options.English=0;
     options.Compile=1;
+    options.PlotData=0;
 end
 
 if nargin <1 % input data not supplied
-     hbode=gcf;
-     return
+    if isempty(get(groot,'CurrentFigure'))
+        % There is no current figure
+        return
+    end
+    hbode=gcf;    
 end
 
 
+%% Prepare folder
+name='bode';
 
+%  Create folder under bodedir to store files
+[s,mess,messid] = mkdir(dirstruct.bodedir, name); % Check here
+dirstruct.bodedirstorage = [dirstruct.bodedir '\' name]; %
 
+assignin('base','dirstruct',dirstruct);
+cd(dirstruct.root)
+save('dirstruct.mat','dirstruct')
+cd(dirstruct.bodedirstorage)
 
-
+%% Get handles
 axesObjs = findobj(hbode,'Type','Axes');  %axes handles
+
+if length(axesObjs)>2
+    warning('Just two axes per figure for now!')
+    return
+end
 
 for i=1:length(axesObjs)
     if ~isempty(strfind(axesObjs(i).YLabel.String,'Phase')) % Find Phase axes
@@ -68,57 +86,95 @@ end
 PhaselinesObjs = findobj(PhaseAxes,'Type','line','Visible','on'); % Find visible lines
 MaglinesObjs = findobj(MagAxes,'Type','line','Visible','on'); % Find visible lines
 
-if length(PhaselinesObjs)>1
-    warning('One bode plot only!')
-    return
-end
+hLegendEntry = legend('show'); % Displays legend and gets handle
+LegendStr=hLegendEntry.String; % Wrong order
 
 
-PhasexData=get(PhaselinesObjs,'Xdata');
-PhaseyData=get(PhaselinesObjs,'Ydata');
 
-MagxData=get(MaglinesObjs,'Xdata');
-MagyData=get(MaglinesObjs,'Ydata');
+
+%% Get axes data
 
 xMPlim = get(MagAxes,'Xlim');
 yMlim = get(MagAxes,'Ylim');
 yPlim = get(PhaseAxes,'Ylim');
-% 
-% figure
-% subplot(2,1,1);
-% semilogx(MagxData,MagyData)
-% grid on
-% xlim(get(MagAxes,'Xlim'))
-% ylim(get(MagAxes,'Ylim'))
-% 
-% subplot(2,1,2);
-% semilogx(PhasexData,PhaseyData)
-% grid on
-% xlim(get(PhaseAxes,'Xlim'))
-% ylim(get(PhaseAxes,'Ylim'))
-% 
-% 
 
 
-name='bode';
+MagYTick = get(MagAxes,'YTick');
+MagYTickLabel = get(MagAxes,'YTickLabel');
+MagYTickstr=['{' strjoin(cellstr(num2str(MagYTick(:))),',') '}'];
+MagYTickLabelstr=['{$' strjoin(MagYTickLabel,'$, $') '$}'];
 
-%  Create folder under bodedir to store files
-[s,mess,messid] = mkdir(dirstruct.bodedir, name); % Check here
-dirstruct.bodedirstorage = [dirstruct.bodedir '\' name]; %
+PhaseYTick = get(PhaseAxes,'YTick');
+PhaseYTickLabel = get(PhaseAxes,'YTickLabel');
+PhaseYTickstr=['{' strjoin(cellstr(num2str(PhaseYTick(:))),',') '}'];
+PhaseYTickLabelstr=['{$' strjoin(PhaseYTickLabel,'$, $') '$}'];
 
-assignin('base','dirstruct',dirstruct);
-cd(dirstruct.root)
-save('dirstruct.mat','dirstruct')
-cd(dirstruct.bodedirstorage)
+PhaseXTick = get(PhaseAxes,'XTick'); 
+PhaseXTickLabel = get(PhaseAxes,'XTickLabel');
 
-% Colect data
-csvdata=[PhasexData(:) MagyData(:) PhaseyData(:)];
-csvheader='freq,mag,phase';
-csvfilename=[dirstruct.bodedirstorage '\' name '.csv'];
-savecsvfile(csvdata, csvheader, csvfilename); % Save to folder
+PhaseXTick=['{' strjoin(cellstr(num2str(PhaseXTick(:))),',') '}'];
+PhaseXTickLabelstr=['{$' strjoin(PhaseXTickLabel,'$, $') '$}'];
 
-bodefile=[name '.csv'];
 
+%% Get plot data
+
+Pcurves=length(PhaselinesObjs);
+Mcurves=length(MaglinesObjs);
+
+if ~isequal(Pcurves,Mcurves)
+    warning('Magnitude and Phase have diferent number os lines!')
+    return
+end
+
+
+for c=1:Pcurves
+    tempdata =  get(PhaselinesObjs(c),'Xdata');
+    PhasexData{c}=tempdata(:);
+    endofxdata{c}=tempdata(end);
+    
+    tempdata=get(PhaselinesObjs(c),'Ydata');
+    PhaseyData{c}=tempdata(:);
+    
+    tempdata=get(MaglinesObjs(c),'Xdata')';
+    MagxData{c}=tempdata(:);
+    tempdata=get(MaglinesObjs(c),'Ydata')';
+    MagyData{c}=tempdata(:);    
+    
+end
+
+
+
+%% Plot figure
+if options.PlotData
+    figure
+    subplot(2,1,1);
+    for c=1:Pcurves
+        semilogx(MagxData{c},MagyData{c})
+        hold all
+    end
+    grid on
+    xlim(get(MagAxes,'Xlim'))
+    ylim(get(MagAxes,'Ylim'))
+    
+    subplot(2,1,2);
+    for c=1:Pcurves
+        semilogx(PhasexData{c},PhaseyData{c})
+        hold all
+    end
+    grid on
+    xlim(get(PhaseAxes,'Xlim'))
+    ylim(get(PhaseAxes,'Ylim'))    
+end
+
+%% Colect data
+for c=1:Pcurves
+    csvdata=[PhasexData{c} MagyData{c} PhaseyData{c}];
+    csvheader='freq,mag,phase';
+    csvfilename{c}=[dirstruct.bodedirstorage '\' name  LegendStr{c} '.csv'];
+    savecsvfile(csvdata, csvheader, csvfilename{c}); % Save to folder
+    
+    bodefile{c}=[name  LegendStr{c} '.csv'];
+end
 
 %% Read predefined files
 
@@ -135,58 +191,59 @@ bodefile=[name '.csv'];
 
 %% Write tex file
 
-     fileoutID = fopen([name '.tex'],'w');
-     fwrite(fileoutID,preamble);
+fileoutID = fopen([name '.tex'],'w');
+fwrite(fileoutID,preamble);
+
+if options.English
+    xlabelstr='Frequency (rad/s)';
+    yMaglabel='Magnitude (dB)';
+    yPhaselabel='Phase (deg)';
+    siunitxstr= '\sisetup{scientific-notation = fixed, fixed-exponent = 0, round-mode = places,round-precision = 2,output-decimal-marker = {.}}';
+else
+    xlabelstr='Frequ\^encia (rad/s)';
+    yMaglabel='Magnitude (dB)';
+    yPhaselabel='Fase (deg)';
+    siunitxstr= '\sisetup{scientific-notation = fixed, fixed-exponent = 0, round-mode = places,round-precision = 2,output-decimal-marker = {,}}';
+end
+
+
+fprintf(fileoutID,'\n%s\n','\nextgroupplot[xmode=log,ymode=normal,height=\axisheight,width=\axiswidth,grid=major,');
+fprintf(fileoutID,'%s\n','minor tick num=5,xtickten = {-10,...,10},xminorgrids=true,xticklabels=\empty,xlabel=\empty,');
+
+fprintf(fileoutID,'%s\n',['yticklabel style={/pgf/number format/fixed},ytick=' MagYTickstr ',yticklabels=' MagYTickLabelstr ',']);
+fprintf(fileoutID,'%s\n',['xmin=' num2str(xMPlim(1)) ',xmax=' num2str(xMPlim(2)) ',ymin='  num2str(yMlim(1)) ',ymax=' num2str(yMlim(2)) ',ylabel=' yMaglabel ',']);
+fprintf(fileoutID,'%s\n','] % End of axis configurations');
+
+fprintf(fileoutID,'\n%s\n','% Settings for siunitx package');
+fprintf(fileoutID,'%s\n',siunitxstr);
+
+for c=1:Pcurves
+    addplotstr=['\addplot[solid,thick,c' num2str(c) 'color]table[x=freq,y=mag,col sep=comma]{' bodefile{c} '}; % Add plot data'];
+    fprintf(fileoutID,'%s\n',addplotstr);
+    fprintf(fileoutID,'%s\n',['\addlegendentry{' LegendStr{c} '};']);
+    fprintf(fileoutID,'%s\n',['\draw[thick] (' num2str(endofxdata{c}) ',' num2str(yMlim(1)) ') -- (' num2str(endofxdata{c}) ',' num2str(yMlim(2)) '); % draw end of data line']);
+end
+
+
+
+fprintf(fileoutID,'\n%s\n','\nextgroupplot[xmode=log,ymode=normal,height=\axisheight,width=\axiswidth,grid=major,');
+fprintf(fileoutID,'%s\n',['minor tick num=5,xtickten = {-10,...,10},xminorgrids=true,xlabel=' xlabelstr ',']);
+fprintf(fileoutID,'%s\n',['yticklabel style={/pgf/number format/fixed},ytick=' PhaseYTickstr ',yticklabels=' PhaseYTickLabelstr ',']);
+fprintf(fileoutID,'%s\n',['xmin=' num2str(xMPlim(1)) ',xmax=' num2str(xMPlim(2)) ',ymin='  num2str(yPlim(1)) ',ymax=' num2str(yPlim(2)) ',ylabel=' yPhaselabel ',']);
+fprintf(fileoutID,'%s\n','] % End of axis configurations');
+
+fprintf(fileoutID,'\n%s\n','% Settings for siunitx package');
+fprintf(fileoutID,'%s\n',siunitxstr);
+
+for c=1:Pcurves
+    addplotstr=['\addplot[solid,thick,c' num2str(c) 'color]table[x=freq,y=phase,col sep=comma]{' bodefile{c} '}; % Add plot data'];
+    fprintf(fileoutID,'%s\n',addplotstr);
+    fprintf(fileoutID,'%s\n',['\addlegendentry{' LegendStr{c} '};']);
+    fprintf(fileoutID,'%s\n',['\draw[thick] (' num2str(endofxdata{c}) ',' num2str(yPlim(1)) ') -- (' num2str(endofxdata{c}) ',' num2str(yPlim(2)) '); % draw end of data line']);
+end
+
      
-     if options.English
-         xlabelstr='Frequency (rad/s)';
-         yMaglabel='Magnitude (dB)';
-         yPhaselabel='Phase (deg)';
-         siunitxstr= '\sisetup{scientific-notation = fixed, fixed-exponent = 0, round-mode = places,round-precision = 2,output-decimal-marker = {.}}';
-     else
-         xlabelstr='Frequ\^encia (rad/s)';
-         yMaglabel='Magnitude (dB)';
-         yPhaselabel='Fase (deg)';
-         siunitxstr= '\sisetup{scientific-notation = fixed, fixed-exponent = 0, round-mode = places,round-precision = 2,output-decimal-marker = {,}}';
-     end
-     
-     
-     
-     fprintf(fileoutID,'\n%s\n','\nextgroupplot[xmode=log,ymode=normal,height=\axisheight,width=\axiswidth,grid=major,');
-     fprintf(fileoutID,'%s\n','minor tick num=5,xtickten = {0,...,10},xminorgrids=true,xticklabels=\empty,xlabel=\empty,');
-     
-     fprintf(fileoutID,'%s\n',['xmin=' num2str(xMPlim(1)) ',xmax=' num2str(xMPlim(2)) ',ymin='  num2str(yMlim(1)) ',ymax=' num2str(yMlim(2)) ',ylabel=' yMaglabel ',']);
-     fprintf(fileoutID,'%s\n','] % End of axis configurations');
-     
-     fprintf(fileoutID,'\n%s\n','% Settings for siunitx package');
-     fprintf(fileoutID,'%s\n',siunitxstr);
-     
-     addplotstr=['\addplot[solid,line width=1pt,c0color]table[x=freq,y=mag,col sep=comma]{' bodefile '}; % Add plot data'];
-     fprintf(fileoutID,'%s\n',addplotstr);
-     
-     fprintf(fileoutID,'%s\n','\addlegendentry{Mag};');
-     
-      
-       
-      
-     
-     fprintf(fileoutID,'\n%s\n','\nextgroupplot[xmode=log,ymode=normal,height=\axisheight,width=\axiswidth,grid=major,');
-     fprintf(fileoutID,'%s\n',['minor tick num=5,xtickten = {0,...,10},xminorgrids=true,xlabel=' xlabelstr ',']);
-     
-     fprintf(fileoutID,'%s\n',['xmin=' num2str(xMPlim(1)) ',xmax=' num2str(xMPlim(2)) ',ymin='  num2str(yPlim(1)) ',ymax=' num2str(yPlim(2)) ',ylabel=' yPhaselabel ',']);
-     fprintf(fileoutID,'%s\n','] % End of axis configurations');
-     
-     fprintf(fileoutID,'\n%s\n','% Settings for siunitx package');
-     fprintf(fileoutID,'%s\n',siunitxstr);
-     
-     addplotstr=['\addplot[solid,line width=1pt,c0color]table[x=freq,y=phase,col sep=comma]{' bodefile '}; % Add plot data'];
-     fprintf(fileoutID,'%s\n',addplotstr);
-     
-     fprintf(fileoutID,'%s\n','\addlegendentry{Phase};');
-     
-     fwrite(fileoutID,endtex);
-     
-     
+     fwrite(fileoutID,endtex);    
      fclose(fileoutID);
      
      
@@ -204,7 +261,9 @@ bodefile=[name '.csv'];
      
  end
  
- 
+  
+ %% Open output directory
+ winopen(dirstruct.bodedirstorage)
  
      
      
--- bode2tikz/index.m
+++ bode2tikz/index.m
@@ -1,14 +1,14 @@
 
-
+%% Example 1 - Single plot
 H = tf([1 0.1 7.5],[1 0.12 9 0 0]);
-Hd = c2d(H,0.5,'zoh');
 bode(H);
+bode2tikz(gcf)
 
-hplot=gcf
-
+%% Example 2 - Continnuous and Discrete plot
 
 H = tf([1 0.1 7.5],[1 0.12 9 0 0]);
 Hd = c2d(H,0.5,'zoh');
 bode(H,Hd)
 
-[mag phase wout] = bode(hplot);
+bode2tikz(gcf)
+
